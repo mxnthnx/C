04e87f562c5577b0ff35f50f9e7ecb5f2e96d12e
MXNTHN <barotmanthan1748@gmail.com>
2026-08-09 19:29:07 +0530

    addition of numbers

--- index.c
+++ index.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World!");
+    printf("Hello World!\n");
+    printf("This is on the new line.\n");
+    printf("My first C program.\n");
     return 0;
 }
