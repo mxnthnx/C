fe8aa13598ccef98993aef0b49cdfedf4013c83e
MXNTHN <barotmanthan1748@gmail.com>
2026-08-13 23:08:22 +0530

    Bullshit

---
+++ codes/grade.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    int studentID, sub1, sub2, sub3, sub4, sub5, totalMarks, totalPercentage;
+
+    printf("Enter your Student ID.");
+    scanf("%d", studentID);
+
+    printf("Enter your Maths marks. (out of 100)");
+    
+    printf("Enter your Physics marks. (out of 100)");
+
+    printf("Enter your Chemistry marks. (out of 100)");
+
+    printf("Enter your English marks. (out of 100)");
+
+    printf("Enter your P.E marks. (out of 100)");
+
+
+
+    return 0;
+}
